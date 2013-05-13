9c9719fc43ab26479da0af01f087e201b1091c24
TejGuthalagowda <tej.guthalagowda@flukenetworks.com>
2013-05-12 23:43:02 -0400

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoxieTube::Application.config.secret_token = 'b98f86baf96b342eec076b7db607b8fa146a05ac981dd75beb7c112a36c3ec0cf9e3967b3235e5a97b64bbd3a9e1ea60e174cee209dda605ac44c02b9c28aab0'
